197eb6b1590dfd2d00de9942011014e4301004a8
IBOCHELLO <yildizibrahim017@gmail.com>
2025-07-25 11:03:02 +0300

    Create BotEggItem.java

---
+++ BotEggItem.java
@@ -0,0 +1,13 @@
+package com.ibochello.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Item.Properties;
+
+public class BotEggItem extends Item {
+
+    public BotEggItem() {
+        super(new Properties().stacksTo(16));
+    }
+
+    // Bot yumurtasının kullanımı ve bot spawn etme işlemi buraya gelecek
+}
