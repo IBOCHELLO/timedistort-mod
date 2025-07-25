3f910139466ed218931d99a9aca8b418a64a9ece
IBOCHELLO <yildizibrahim017@gmail.com>
2025-07-25 10:46:29 +0300

    Create TimedistortMod.java

---
+++ src/main/java/com/ibochello/TimedistortMod.java
@@ -0,0 +1,12 @@
+package com.ibochello;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("timedistort")
+public class TimedistortMod {
+    public static final String MODID = "timedistort";
+
+    public TimedistortMod() {
+        // Mod başlangıç kodları buraya
+    }
+}
